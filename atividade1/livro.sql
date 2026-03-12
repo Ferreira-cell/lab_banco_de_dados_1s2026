73e7aa36ceea56bebf90ade8c3605398388800c8
Ferreira-cell <vtinaugusto17@gmail.com>
2026-03-11 21:12:58 -0300

    Update livro.sql

--- atividade1/livro.sql
+++ atividade1/livro.sql
@@ -3,9 +3,9 @@ create table livro(
 	id_livro int auto_increment primary key,
     titulo varchar(255) not null,
     sinopse varchar(255) not null,
-    autos varchar(255) not null,
-    editora varchar(255) not null,
-    categoria varchar(255) not null,
+    autos varchar(128) not null,
+    editora varchar(128) not null,
+    categoria varchar(128) not null,
     ano_publicacao year
 );
 show tables;
