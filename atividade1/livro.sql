1e31e86a6cf88a1e52dee6116769545a352c032c
Ferreira-cell <vtinaugusto17@gmail.com>
2026-03-11 21:13:19 -0300

    Update livro.sql

--- atividade1/livro.sql
+++ atividade1/livro.sql
@@ -2,7 +2,7 @@ use bibliotaca_usuario_1s2026;
 create table livro(
 	id_livro int auto_increment primary key,
     titulo varchar(255) not null,
-    sinopse varchar(255) not null,
+    sinopse text not null,
     autos varchar(128) not null,
     editora varchar(128) not null,
     categoria varchar(128) not null,
