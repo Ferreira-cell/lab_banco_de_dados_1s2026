8ac2ce0464d0efc08e0771babd66a330ca7dd1b5
Ferreira-cell <vtinaugusto17@gmail.com>
2026-03-11 21:14:23 -0300

    Update livro.sql

--- atividade1/livro.sql
+++ atividade1/livro.sql
@@ -6,6 +6,6 @@ create table livro(
     autos varchar(128) not null,
     editora varchar(128) not null,
     categoria varchar(128) not null,
-    ano_publicacao year
+    ano_publicacao year not null
 );
 show tables;
