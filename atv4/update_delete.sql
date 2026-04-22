2667db31d163162d4de81c686fb32e6f6809018f
Ferreira-cell <vtinaugusto17@gmail.com>
2026-04-22 20:27:19 -0300

    Update update_delete.sql

--- atv4/update_delete.sql
+++ atv4/update_delete.sql
@@ -1,6 +1,6 @@
 select * from autor;
 select * from livro where id_autor = 8;
-
+select * from livro;
 select * from categoria;
 SELECT * from livro where id_categoria = 6;
 
@@ -11,6 +11,37 @@ SELECT id_categoria FROM categoria WHERE nome = 'Censurado';
 SELECT * FROM categoria WHERE nome = 'Censurado';
 SELECT * FROM livro WHERE id_categoria = 6;
 SELECT * FROM livro;
+
+/* Carrega livro e nome do autor*/
+select 
+			t4.nome as nome_usuario,
+			t2.nome as nome_autor,
+			t1.titulo,
+            t3.nome as nome_cateogoria,
+            
+            t5.nome as nome_editora
+    from 
+			livro t1
+	join 
+		autor t2 on (t1.id_autor = t2.id_autor)
+	join
+		categoria t3 on (t1.id_categoria = t3.id_categoria)
+	join
+		usuario t4 on (t1.id_usuario = t4.id_usuario)
+	join
+		editora t5 on (t1.id_editora = t5.id_editora)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
