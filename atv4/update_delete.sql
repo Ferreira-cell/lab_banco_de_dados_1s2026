2d18903db5ee2a44a7b1aa0005697fe94e3e096e
Ferreira-cell <vtinaugusto17@gmail.com>
2026-04-29 20:59:45 -0300

    Update update_delete.sql

--- atv4/update_delete.sql
+++ atv4/update_delete.sql
@@ -29,27 +29,34 @@ select
 	join
 		usuario t4 on (t1.id_usuario = t4.id_usuario)
 	join
-		editora t5 on (t1.id_editora = t5.id_editora)
+		editora t5 on (t1.id_editora = t5.id_editora);
+
+/* qntd de livro*/
+
+select count(*) from livro where id_usuario = 1;
+select count(*) from livro where id_usuario = 2;
+select count(*) from livro where id_usuario = 3;
+
+select count(*) from livro group by id_usuario;
 
 
+select 
+	t1.id_usuario, 
+	t2.nome as ususario, 
+	t1.id_categoria,
+    t3.nome as categoria,
+	count(t1.id_usuario) 
+as 
+	qtd_livro 
+from 
+	livro t1
+join 
+	usuario t2 on (t1.id_usuario = t2.id_usuario) 
+join 
+	categoria t3 on (t1.id_categoria = t3.id_categoria) 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+group by 
+	t1.id_usuario, t2.nome, t1.id_categoria, t3.nome
+    
+order by t3.nome, qtd_livro desc;
 
